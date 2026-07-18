571e4d9bc5378e0a9b00c4be9165261d5527d18c
Zennthenics <ya0632326@gmail.com>
2026-07-18 21:15:27 +0200

    removed cursore

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -1,39 +1,4 @@
-const cursor = document.querySelector(".cursor");
 
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
-
-let currentX = mouseX;
-let currentY = mouseY;
-
-// Change this:
-// 0.08 = floaty
-// 0.12 = smooth
-// 0.18 = responsive
-// 0.25 = almost instant
-const speed = 0.18;
-
-document.addEventListener("mousemove", (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-});
-
-function animate() {
-
-    currentX += (mouseX - currentX) * speed;
-    currentY += (mouseY - currentY) * speed;
-
-    // Adjust these numbers until the sword tip is exactly where you click
-    const offsetX = -6;
-    const offsetY = -6;
-
-    cursor.style.transform =
-        `translate3d(${currentX + offsetX}px, ${currentY + offsetY}px, 0)`;
-
-    requestAnimationFrame(animate);
-}
-
-animate();
 window.addEventListener("load",()=>{
 
     const loader = document.getElementById("loader");
